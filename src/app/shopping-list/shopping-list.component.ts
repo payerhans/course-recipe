7506d5c402c89fec7c3b121d173f2b50d2063695
payerhans <32729314+payerhans@users.noreply.github.com>
2017-12-13 16:51:52 +0100

    52 creating and outputting the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('tomatoes', 2),
+    new Ingredient('veal', 1),
+    new Ingredient('peas', 7),
+    new Ingredient('pepper', 1),
+  ];
 
   constructor() { }
 
